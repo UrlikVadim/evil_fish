d63cac50c84d0a49b0b1f550b2b8bbb5f49210b4
Vadim <urlikvadim@mail.ru>
2019-01-05 22:05:16 +0300

    create ORM model

--- evilfish_admin/models.py
+++ evilfish_admin/models.py
@@ -4,3 +4,51 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Admins(models.Model):
+    login = models.TextField()
+    passw = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.login
+
+
+class Logo(models.Model):
+    phones = models.TextField()
+    urlvideo = models.TextField()
+    mainhtml = models.TextField()
+
+    def __str__(self):
+        return self.phones
+
+
+class Category(models.Model):
+    name = models.TextField()
+    vegan = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey("Category")
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    imageurl = models.TextField()
+    visible = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+    name = models.TextField()
+    comment = models.TextField()
+    email = models.EmailField()
+    visible = models.BooleanField()
+
+    def __str__(self):
+        return self.name
