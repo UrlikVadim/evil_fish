2b860324d23647192148af4dd54ad4404862e186
Vadim <urlikvadim@mail.ru>
2019-01-08 23:35:58 +0300

    add some function

--- evilfish_admin/urls.py
+++ evilfish_admin/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from . import views
+# admin/
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login$', views.login),
+    url(r'^getLogo$', views.getLogo),
+    url(r'^getCategory', views.getCategory),
+    url(r'^getProduct', views.getProduct),
+    url(r'^getComments', views.getComments),
+    url(r'^setLogo', views.setLogo),
+    url(r'^setCategory', views.setCategory),
 ]
--- evilfish_admin/views.py
+++ evilfish_admin/views.py
@@ -2,7 +2,86 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.http import HttpResponse
+from models import Logo, Category, Product, Comments
 # Create your views here.
-def index():
-    pass
+
+
+def index(request):
+    return render(request, "admin.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def getLogo(request):
+    out = Logo.objects.get(pk=1)
+    resp = {
+        "phone": out.phones,
+        "urlvideo": out.urlvideo,
+        "mainhtml": out.mainhtml
+    }
+    return JsonResponse(dict(resp))
+
+
+def getCategory(request):
+    out = list(Category.objects.all().values())
+    return JsonResponse({"data": out})
+
+
+def getProduct(request):
+    out = list(Product.objects.filter(category=request.GET['category']).values())
+    return JsonResponse({"data": out})
+
+
+def getComments(request):
+    out = list(Comments.objects.all().values())
+    return JsonResponse({"data": out})
+
+
+def setLogo(request):
+    if request.method == "POST":
+        logo = Logo.objects.get(pk=1)
+        logo.phones = request.POST['phone']
+        logo.urlvideo = request.POST['urlvideo']
+        logo.mainhtml = request.POST['mainhtml']
+        logo.save()
+        return HttpResponse('Обновление успешшно', content_type='text/plain')
+    else:
+        return render(request, "login.html")
+
+
+def setCategory(request):
+    if request.method == "POST":
+        if request.POST['typeOperation'] == "add":
+            newCategory = Category()
+            newCategory.name = request.POST['name']
+            newCategory.vegan = bool(int(request.POST['vegan']))
+            newCategory.save()
+            return HttpResponse('Добавление успешшно', content_type='text/plain')
+        elif request.POST['typeOperation'] == "upd":
+            idCat = int(request.POST['id'])
+            updCategory = Category.objects.get(pk=idCat)
+            updCategory.name = request.POST['name']
+            updCategory.vegan = bool(int(request.POST['vegan']))
+            updCategory.save()
+            return HttpResponse('Обновление успешшно', content_type='text/plain')
+        elif request.POST['typeOperation'] == "del":
+            idCat = int(request.POST['id'])  # TODO добавить удаление фото по ссылке
+            delCategory = Category.objects.get(pk=idCat)
+            delCategory.delete()
+            return HttpResponse('Удаление успешшно', content_type='text/plain')
+        else:
+            return HttpResponse('Ошибка операции', content_type='text/plain', status=400)
+    else:
+        return render(request, "login.html")
+
+
+def setProduct(request):
+    return render(request, "login.html")
+
+
+def setComments(request):
+    return render(request, "login.html")
