6d7634435c5aa8dd6fd6b3e15e13b804e19ff529
Vadim <urlikvadim@mail.ru>
2019-05-26 22:27:41 +0300

    add route favicon

--- evilfish_main/urls.py
+++ evilfish_main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     url('^$', views.index),
     url('^login', views.login),
     url('^blockspam', views.blockspam),
